693510bc2dcf2e9e2b5f026a3e64506e4d62c33e
huangxiaojing <huangxiaojingcn@gmail.com>
2019-01-02 10:31:03 +0800

    Feat: 初始化项目

---
+++ app/src/main/java/com/archpoem/poemlist/ui/PeomDetailFragment.java
@@ -0,0 +1,24 @@
+package com.archpoem.poemlist.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by huangxiaojing on 02/01/2019.
+ */
+
+public class PeomDetailFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
